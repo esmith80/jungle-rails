297cbc48f73917882b06c107806a973e90909f39
Edward Smith <edward.m.smith80@gmail.com>
2020-10-14 23:11:12 +0000

    admin dashboard feature complete

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,5 +3,10 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD']
 
   def show
+    # number of products in database
+    @product_count = Product.count
+    # number of categories in database
+    @category_count = Product.select(:category_id).distinct.count
+
   end
 end
