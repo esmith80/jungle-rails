4bddf75dc7756fbecba1105eb273f9623dd2e558
Edward Smith <edward.m.smith80@gmail.com>
2020-10-20 04:30:56 +0000

    added all validations and moved user validation logic to User class

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,20 @@ class User < ActiveRecord::Base
 
   has_secure_password
   
+  validates :name, presence: true
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
+  validates :password, length: { minimum: 8 } 
+  validates :email, uniqueness: { case_sensitive: false }
+  validates :email, presence: true
+
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by_email(email.strip.downcase)
+    if user && user.authenticate(password)
+      user
+    else
+      nil
+    end
+  end
 end
+
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,143 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+
+  describe 'Validations' do
+  it 'should create a new user with all fields completed' do
+    @user = User.new(
+    name: "Alice Test",
+    email: "test@test.com",
+    password:"password",
+    password_confirmation:"password"
+    )
+    @user.save
+    expect(@user.errors.full_messages).to be_empty
+  end
+
+  it 'should receive an error message if name is not present' do
+    @user = User.new(
+      name: nil,
+      email: "test@test.com",
+      password:"password",
+      password_confirmation:"password"
+      )
+    @user.save
+    expect(@user.errors.full_messages).to include("Name can't be blank")
+  end
+  
+  it 'should receive an error message if email is not present' do
+    @user = User.new(
+      name: "Alice Test",
+      email: nil,
+      password:"password",
+      password_confirmation:"password"
+      )
+    @user.save
+    expect(@user.errors.full_messages).to include("Email can't be blank")
+  end
+
+  it 'should receive and error if email is already in database' do
+    @user = User.new(
+      name: "Alice Test",
+      email: "test@test.com",
+      password:"password",
+      password_confirmation:"password"
+      )
+    @user.save
+    @user_two = User.new(
+      name: "Alice Test",
+      email: "test@test.com",
+      password:"password",
+      password_confirmation:"password"
+      )
+    @user_two.save
+    expect(@user_two.errors.full_messages).to include("Email has already been taken")
+  end
+
+  it 'should receive and error if email is already in database' do
+    @user = User.new(
+      name: "Alice Test",
+      email: "test@test.com",
+      password:"password",
+      password_confirmation:"password"
+      )
+    @user.save
+    @user_two = User.new(
+      name: "Alice Test",
+      email: "test@TEST.com",
+      password:"password",
+      password_confirmation:"password"
+      )
+    @user_two.save
+    expect(@user_two.errors.full_messages).to include("Email has already been taken")
+  end
+
+  it 'should receive an error message if password length is less than 8 characters' do
+    @user = User.new(
+      name: nil,
+      email: "test@test.com",
+      password:"passwor",
+      password_confirmation:"passwor"
+      )
+    @user.save
+    expect(@user.errors.full_messages).to include("Password is too short (minimum is 8 characters)")
+  end
+end
+
+  describe '.authenticate_with_credentials' do
+    # examples for this class method here
+    it 'should return a user if authenticated successfully' do
+      @user = User.new(
+      name: "Alice Test",
+      email: "test@test.com",
+      password:"password",
+      password_confirmation:"password"
+      )
+      @user.save
+      @authenticated_user = User.authenticate_with_credentials("test@test.com", "password")
+      
+      expect(@user).to eq(@authenticated_user)
+    end
+
+    it 'should return nil if authentication fails' do
+      @user = User.new(
+      name: "Alice Test",
+      email: "test@test.com",
+      password:"password",
+      password_confirmation:"password"
+      )
+      @user.save
+      @authenticated_user = User.authenticate_with_credentials("test@test.com", "pasword")
+      
+      expect(@authenticated_user).to eq(nil)
+    end
+
+    it 'should allow extra spaces in login email' do
+      @user = User.new(
+      name: "Alice Test",
+      email: "test@test.com",
+      password:"password",
+      password_confirmation:"password"
+      )
+      @user.save
+      @authenticated_user = User.authenticate_with_credentials("   test@test.com", "password")
+      
+      expect(@user).to eq(@authenticated_user)
+    end
+
+    it 'email validation should be case insensitive when logging in' do
+      @user = User.new(
+      name: "Alice Test",
+      email: "test@test.com",
+      password:"password",
+      password_confirmation:"password"
+      )
+      @user.save
+      @authenticated_user = User.authenticate_with_credentials("TEST@TEST.com", "password")
+      
+      expect(@user).to eq(@authenticated_user)
+    end
+
+  end
+
+end
